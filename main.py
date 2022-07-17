ee6fa5142a657813b4293ddc0698a46eb481df64
Beatrice Federici <beatrice.federici123@gmail.com>
2022-07-17 21:43:29 +0100

    added more edge cases to rhyming function

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 import requests
 from bs4 import BeautifulSoup
+import re
 
 
 
@@ -10,24 +11,45 @@ from bs4 import BeautifulSoup
 BASE_URL= "https://rhymer.com/"
 
 
-def scrape_page(word):
+
+def scrape_page(word, j=20):
 
     """When a user imputs a word in rhymer.com, they are redirected to a page
     which displays all the words that rhyme with the word the user imputted. This function
-    scrapes all the rhymes present in that page."""
+    scrapes all the rhymes present in that page and returns a list of unclean data"""
 
 
     html_text = requests.get(BASE_URL + f"{word}.html").text
     soup = BeautifulSoup(html_text, "html.parser")
-    words = soup.find_all("a", class_= "btn c")
+
+    unclean_data = []
+    for i in range (1,j+1):
+
+        word = soup.find_all("div", id= f"syl-{i}")
+        if len(word) != 0:
+            unclean_data.append(word)
+
+    return unclean_data
+
+
+
+def clean_data(unclean_data):
+
+    """This function takes the unclean list of data returned by the scrape_data
+    function and cleans it, putting the words into a lis."""
 
     rhyming_words = []
+    for i in range(0, len(unclean_data) -1):
 
-    for i in range(len(words) - 1):
-        rhyme = words[i]
-        rhyme = rhyme.text
+        word = str(unclean_data[i])
+        newstring = re.sub(r"[^a-zA-Z]+", "", word)
+        newstring = newstring.split("html")
 
-        rhyming_words.append(rhyme)
+        for string in newstring[:-1]:
+            string = string.split("href")
+
+            newstring[-1].rstrip("div")
+            rhyming_words.append(string[1])
     return rhyming_words
 
 
@@ -35,19 +57,31 @@ def scrape_page(word):
 
 def find_rhyme(word, syllables=None):
 
+
     """This is a function that uses the data scraped from the scraper function
     and presents it into a nice list of words that rhyme. It takes two parameters, the word
     the user would like to find rhyming words for and an optional parameter,
     the syllable parameter. There the user can input an integer that specifies how many
     syllables they woud like the rhyming word to have. If not specified the function
-    will return a list of all the words that rhyme witht eh """
+    will return a list of all the words that rhyme with it """
 
 
     if  isinstance(word, str):
-        if not syllables:
-            return scrape_page(word)
-        else:
-            pass
+        if syllables == None:
+            unclean_data = scrape_page(word)
+            return clean_data(unclean_data)
+
+        if  not isinstance(syllables, int):
+            raise ValueError("Non integer types are not allowed here")
+
+        if  syllables < 1:
+            raise ValueError("Integer for syllables must be positive")
+
+        if syllables >= 1:
+            unclean_data= scrape_page(word, syllables)[-1]
+            return unclean_data
+
+
     else:
         raise ValueError("Non string types are not allowed here")
 
@@ -55,7 +89,7 @@ def find_rhyme(word, syllables=None):
 
 
 
-#print(scrape_page("horse"))
 
+print(find_rhyme("case", -1))
 
 #print(type("horse"))
